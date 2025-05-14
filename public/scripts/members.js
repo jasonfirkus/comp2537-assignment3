2dc8eea4953e7e5e4c9c6d389435d7b236ef7975
jason <firkus.jason@gmail.com>
2025-05-14 03:16:36 -0700

    add game state

--- public/scripts/members.js
+++ public/scripts/members.js
@@ -1,5 +1,13 @@
 const NUM_POKEMON = 1302;
 
+const GAME_STATE = {
+  TOTAL_PAIRS: 3,
+  PAIRS_LEFT: 3,
+  COUNT_OF_FLIPS: 0,
+  PAIRS_MATCHED: 0,
+  LAST_FLIPPED: [],
+};
+
 /**
  * Returns a Promise that resolves to an array of objects containing the name and official artwork URL of a set of random Pokemons.
  * The amount parameter specifies the number of Pokemons to return, between 1 and 1302.
@@ -35,11 +43,12 @@ async function createPokemon(amount = 6) {
     const template = document.querySelector("template");
     const clone = template.content.cloneNode(true);
 
-    const front = clone.querySelector(".front");
+    const front = clone.querySelector(".front img");
     front.src = imageUrl;
     front.alt = name;
 
     clone.querySelector("div").dataset.pokemon = name;
+    clone.querySelector("p").textContent = name;
 
     return clone;
   });
@@ -52,12 +61,36 @@ async function createPokemon(amount = 6) {
   }
 
   document.getElementById("board").appendChild(df);
+  GAME_STATE.TOTAL_PAIRS = amount / 2;
+  GAME_STATE.PAIRS_LEFT = amount / 2;
 }
 
 await createPokemon();
 document.querySelectorAll(".inner").forEach((card) =>
   card.addEventListener("click", (event) => {
-    card.classList.toggle("flipped");
+    if (card.classList.contains("flipped")) return;
+
+    console.log("GAME_STATE", GAME_STATE);
+
+    card.classList.add("flipped");
+    GAME_STATE.COUNT_OF_FLIPS++;
+    GAME_STATE.LAST_FLIPPED.push(card);
+
+    if (GAME_STATE.LAST_FLIPPED.length != 2) return;
+
+    const [firstCard, secondCard] = GAME_STATE.LAST_FLIPPED;
+
+    if (firstCard.parentElement.dataset.pokemon == secondCard.parentElement.dataset.pokemon) {
+      GAME_STATE.PAIRS_LEFT--;
+      GAME_STATE.PAIRS_MATCHED++;
+      GAME_STATE.LAST_FLIPPED = [];
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        GAME_STATE.LAST_FLIPPED = [];
+      }, 1000);
+    }
   })
 );
 document.getElementById("loader-wrapper").hidden = true;
